89851fad849d81f9f25a6cf5f8e9429624649d72
NCKalyani <111537246+NCKalyani@users.noreply.github.com>
2023-06-30 06:35:59 +0000

    added import stmt

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,8 @@
-def palindrome(string):
-  forward = ''.join(re.findall(r'[a-z]',string.lower()))
-  backward = forward[::-1]
-  return forward == backward
+import re
+def palindrome(phrase):
+    forward = ''.join(re.findall(r'[a-z]',phrase.lower()))
+    backward = forward[::-1]
+    return forward == backward
+
+if __name__ == '__main__':
+    palindrome("LEvel")
